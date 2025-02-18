f4bb9f60798c39e80b196b068545191a25919936
M. B. Mahir Tanzim <tanzim15-5400@diu.edu.bd>
2025-02-18 20:59:03 +0600

    task_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,97 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const LabClass05());
+void main() {
+  runApp(App());
 }
 
-class LabClass05 extends StatelessWidget{
-  const LabClass05({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title : "Basic Flutter UI - 02",
+      title: "Basic UI - 02",
       home: Scaffold(
         appBar: AppBar(
-          
-          title: Text("Basic Flutter UI - 2"),
-          centerTitle: true,
-          backgroundColor:  const Color.fromARGB(155, 36, 2, 254),
-
-          elevation: 23,
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.all(50),
-            padding: EdgeInsets.all(50),
-            color: Colors.green,
-            height: 600,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Text("50% off"),
-                    Icon(
-                      Icons.emoji_emotions,
-                      size: 30,
-                      color: Colors.grey,
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("Nike air shoes")
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("450"),
-                    Text("2001"),
-                  ],
-                )
-              ],
-            )
+          title: Text("Basic UI - 02"),
+          backgroundColor: Colors.blue,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
           ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 244, 244, 244),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(
+                          "50% OFF",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.local_hospital_rounded,
+                        size: 40,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Image.network(
+                      "https://nikearprod.vtexassets.com/arquivos/ids/1217812-800-800?width=800&height=800&aspect=true",
+                    ),
+                  ),
+                  Text(
+                    "Nike Air Max",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "\$45,000",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text(
+                          "\$50,000",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
